c42ff7aede49b09cbd60059c766aa3cb09687e2a
Kodi <soiler@forsythe.com>
2014-08-25 20:48:08 -0600

    Refinements that didn't make it to last commit

--- docroot/WEB-INF/src/org/oilers/liferay/activity/MyActivityPortlet.java
+++ docroot/WEB-INF/src/org/oilers/liferay/activity/MyActivityPortlet.java
@@ -4,31 +4,22 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Enumeration;
-import java.util.List;
-import java.util.Map;
 
 import javax.portlet.PortletException;
-import javax.portlet.PortletRequest;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
 import com.liferay.portal.kernel.dao.orm.DynamicQuery;
-import com.liferay.portal.kernel.dao.orm.DynamicQueryFactoryUtil;
 import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.model.User;
-import com.liferay.portal.service.UserLocalServiceUtil;
 import com.liferay.portal.util.PortalUtil;
-import com.liferay.portlet.blogs.model.BlogsEntry;
 import com.liferay.portlet.blogs.service.BlogsEntryLocalServiceUtil;
-import com.liferay.portlet.blogs.service.BlogsEntryServiceUtil;
 import com.liferay.portlet.expando.model.ExpandoBridge;
 import com.liferay.portlet.expando.model.ExpandoColumnConstants;
 import com.liferay.portlet.journal.model.JournalArticle;
-import com.liferay.portlet.journal.service.JournalArticleService;
 import com.liferay.portlet.journal.service.JournalArticleServiceUtil;
-import com.liferay.portlet.journal.service.persistence.JournalArticleFinderUtil;
-import com.liferay.portlet.journal.service.persistence.JournalArticleUtil;
 import com.liferay.util.bridges.mvc.MVCPortlet;
 
 /**
@@ -39,11 +30,12 @@ public class MyActivityPortlet extends MVCPortlet {
 	private static final String TEMPLATE_ARTICLE_ID = "11461";
 	private static final long TEMPLATE_GROUP_ID = 10184L;
 	private static final String LOGIN_PROPERTY = "logins";
+	
+	private static final Log log = LogFactoryUtil.getLog(MyActivityPortlet.class);
 
 	@Override
 	public void render(RenderRequest request, RenderResponse response)
 			throws PortletException, IOException {
-		request.getPreferences().setValue("testParam", "This is the song that never ends");
 		Integer logins = -1;
 		
 		try {
@@ -67,11 +59,9 @@ public class MyActivityPortlet extends MVCPortlet {
 						.replace("]]>", "");
 				request.getPreferences().setValue("content", content);
 			}
-			
 		} catch(Exception e) {
-			e.printStackTrace();
+			log.error(e);
 		}
-		
 		super.render(request, response);
 	}
 	
