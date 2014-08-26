5e6fb8c6ad8fb7a4e21b26f859561779f9fc7698
Kodi <soiler@forsythe.com>
2014-08-25 20:47:43 -0600

    Looking up blogs from a service and using log

--- docroot/WEB-INF/src/org/oilers/liferay/activity/MyActivityPortlet.java
+++ docroot/WEB-INF/src/org/oilers/liferay/activity/MyActivityPortlet.java
@@ -13,9 +13,17 @@ import javax.portlet.PortletRequest;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
+import com.liferay.portal.kernel.dao.orm.DynamicQuery;
+import com.liferay.portal.kernel.dao.orm.DynamicQueryFactoryUtil;
+import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
 import com.liferay.portal.model.User;
 import com.liferay.portal.service.UserLocalServiceUtil;
 import com.liferay.portal.util.PortalUtil;
+import com.liferay.portlet.blogs.model.BlogsEntry;
+import com.liferay.portlet.blogs.service.BlogsEntryLocalServiceUtil;
+import com.liferay.portlet.blogs.service.BlogsEntryServiceUtil;
+import com.liferay.portlet.expando.model.ExpandoBridge;
+import com.liferay.portlet.expando.model.ExpandoColumnConstants;
 import com.liferay.portlet.journal.model.JournalArticle;
 import com.liferay.portlet.journal.service.JournalArticleService;
 import com.liferay.portlet.journal.service.JournalArticleServiceUtil;
@@ -28,28 +36,34 @@ import com.liferay.util.bridges.mvc.MVCPortlet;
  */
 public class MyActivityPortlet extends MVCPortlet {
  
+	private static final String TEMPLATE_ARTICLE_ID = "11461";
+	private static final long TEMPLATE_GROUP_ID = 10184L;
+	private static final String LOGIN_PROPERTY = "logins";
+
 	@Override
 	public void render(RenderRequest request, RenderResponse response)
 			throws PortletException, IOException {
 		request.getPreferences().setValue("testParam", "This is the song that never ends");
 		Integer logins = -1;
-		Integer blogs = -1;
 		
 		try {
-			
-			JournalArticle rightArticle = JournalArticleServiceUtil.getArticle(10184L, "11461");
+			JournalArticle rightArticle = JournalArticleServiceUtil.getArticle(TEMPLATE_GROUP_ID, TEMPLATE_ARTICLE_ID);
 			if(rightArticle != null) {
 				String content = rightArticle.getContent();
 				DateFormat format = new SimpleDateFormat("MM/dd/YYYY");
 				User user = PortalUtil.getUser(request);
-				logins = (Integer) user.getExpandoBridge().getAttribute("logins");
-				request.getPreferences().setValue("logins", logins.toString());
-				blogs = (Integer) user.getExpandoBridge().getAttribute("blogs");
-				request.getPreferences().setValue("blogs", logins.toString());
+				DynamicQuery dynamicQuery = BlogsEntryLocalServiceUtil.dynamicQuery();
+				dynamicQuery.add(PropertyFactoryUtil.forName("userId").eq(user.getUserId()));
+				Long blogs = BlogsEntryLocalServiceUtil.dynamicQueryCount(dynamicQuery);
+				ExpandoBridge expandoBridge = user.getExpandoBridge();
+				if(!user.getExpandoBridge().hasAttribute(LOGIN_PROPERTY)) {
+					user.getExpandoBridge().addAttribute(LOGIN_PROPERTY, ExpandoColumnConstants.INTEGER, 0);
+				}
+				logins = (Integer) expandoBridge.getAttribute(LOGIN_PROPERTY);
 				content = content.replace("[FULL_NAME]", user.getFullName())
 						.replace("[TODAYS_DATE]", format.format(new Date()))
 						.replace("[NUM_LOGINS]", logins.toString())
-						.replace("[NUM_CREATED_BLOGS]", blogs.toString())
+						.replace("[NUM_CREATED_BLOGS]", blogs + "")
 						.replace("]]>", "");
 				request.getPreferences().setValue("content", content);
 			}
